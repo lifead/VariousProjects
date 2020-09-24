05ded9359a9dd89cc8e586379ca23bb5bbd40a23
evasilyev <evasilyev@EVASILYEV-INET>
2020-09-24 12:56:36 +0300

    Add Test Project

---
+++ Web20200924/Web20200924/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web20200924.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
